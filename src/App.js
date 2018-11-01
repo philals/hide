7e77d0b99a6c780c89b427f7d3e29f767253c10a
Phil Alsford <phil.alsford@xero.com>
2018-11-02 12:54:00 +1300

    Adds banner
    Co-authored-by: Matthew Mortimer <matthew.mortimer@xero.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import GitHubForkRibbon from 'react-github-fork-ribbon';
 import LocationState from "./LocationState";
 
 class App extends Component {
   render() {
     return (
       <div>
+         <GitHubForkRibbon href="https://github.com/philals/hide"
+                    target="_blank"
+                    position="right">Fork me</GitHubForkRibbon>
         <LocationState />
       </div>
     );
